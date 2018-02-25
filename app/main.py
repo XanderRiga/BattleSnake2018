f8aad5a88b3a5ed9a676f2df25bb50bf1936869a
Xander Riga <xanderriga@live.com>
2018-02-24 18:00:16 -0800

    fixed basecase of recursion

--- app/main.py
+++ app/main.py
@@ -113,22 +113,28 @@ def printmatrix(matrix):
 def floodfill(matrix, point):
     y = point['x']
     x = point['y']
+    count = 0
     if matrix[x][y] == 0:
         matrix[x][y] = 1
 
         if x > 0 and matrix[x-1][y] != 1:
+            count += 1
             return floodfill(matrix, getleft(point))
         if x < len(matrix)-1 and matrix[x+1][y]:
+            count += 1
             return floodfill(matrix, getright(point))
         if y > 0 and matrix[x][y-1] != 1:
+            count += 1
             return floodfill(matrix, getup(point))
         if y < len(matrix[0])-1 and matrix[x][y+1]:
+            count += 1
             return floodfill(matrix, getdown(point))
 
-    print('matrix after floodfill')
-    printmatrix(matrix)
-    print('matrix 0 count after fill: ' + str(countmatrix0(matrix)))
-    return countmatrix0(matrix)
+    if count == 0:
+        print('matrix after floodfill')
+        printmatrix(matrix)
+        print('matrix 0 count after fill: ' + str(countmatrix0(matrix)))
+        return countmatrix0(matrix)
 
 
 def countmatrix0(matrix):
@@ -308,17 +314,21 @@ def isadjacentdiagonal(a, b):
     else:
         return False
 
+
 def getleft(point):
     point['x'] -= 1
     return point
+
 
 def getright(point):
     point['x'] += 1
     return point
 
+
 def getup(point):
     point['y'] -= 1
     return point
+
 
 def getdown(point):
     point['y'] += 1
