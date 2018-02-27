96924ad4a92533e9bd2323f40183ac9e864983d8
Xander Riga <xanderriga@live.com>
2018-02-26 23:08:00 -0800

    removed returns

--- app/main.py
+++ app/main.py
@@ -124,13 +124,13 @@ def floodfill(matrix, x, y, width, height, list):
         list.append(1)
 
         if x > 0:
-            return floodfill(matrix, x-1, y, width, height, list)
+            floodfill(matrix, x-1, y, width, height, list)
         if x < height-1:
-            return floodfill(matrix, x+1, y, width, height, list)
+            floodfill(matrix, x+1, y, width, height, list)
         if y > 0:
-            return floodfill(matrix, x, y-1, width, height, list)
+            floodfill(matrix, x, y-1, width, height, list)
         if y < width-1:
-            return floodfill(matrix, x, y+1, width, height, list)
+            floodfill(matrix, x, y+1, width, height, list)
     else:
         print('There are no directions to go from x: ' + str(x) + "y: " + str(y))
         printmatrix(matrix)
